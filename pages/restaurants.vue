9b8648134bcdd1c29b1d88900398d7bc4ada2eff
Amansingh1202 <aks001235@gmail.com>
2021-01-26 15:36:06 +0530

    added restaurants page

---
+++ pages/restaurants.vue
@@ -0,0 +1,10 @@
+<template>
+<app-restaurant-info></app-restaurant-info>
+</template>
+
+<script>
+import AppRestaurantInfo from '../components/AppRestaurantInfo.vue'
+    export default {
+        components: { AppRestaurantInfo },
+    }
+</script>
