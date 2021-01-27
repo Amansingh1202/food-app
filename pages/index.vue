b2f150e451887e28c85aeb3d2e56bc7c2b41259e
Amansingh1202 <aks001235@gmail.com>
2021-01-27 15:29:25 +0530

    fixed home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,25 @@
 <template>
   <main class="container">
     <app-header></app-header>
-    <app-restaurant-info></app-restaurant-info>
+    <app-restaurant-info :datasource="fooddata"></app-restaurant-info>
   </main>
 </template>
 
 <script>
+
+import { mapState } from 'vuex';
+
 import AppHeader from '@/components/AppHeader.vue';
 import AppRestaurantInfo from '../components/AppRestaurantInfo.vue';
 export default {
   components:{
     AppHeader,
     AppRestaurantInfo,
+  },
+  computed:{
+    ...mapState([
+      'fooddata'
+    ])
   }
 }
 </script>
